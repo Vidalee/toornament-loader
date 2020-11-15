3594c8a170211b53b982661b8e54f4a5fcadd090
Vidalee <thomas.sirvent@free.fr>
2020-11-15 18:25:35 +0100

    added stats generation

--- toornament.js
+++ toornament.js
@@ -13,6 +13,7 @@ const r = require("rethinkdbdash")({
 });
 
 const config = require("./config.json");
+const fs = require('fs')
 
 const cookies = config.cookies;
 const app = require('express')();
@@ -22,11 +23,11 @@ const api_key = config.toornament_api_key;
 
 const riot_key = config.riot_key;
 const port = 3003;
-const callbackUrl = 'http://localhost:3003/callback';
+const callbackUrl = config.callbackUrl;
 let token;
 const { AuthorizationCode } = require('simple-oauth2');
 const axios = require('axios');
-const { range } = require('lodash');
+const { range, lowerFirst } = require('lodash');
 
 const client = new AuthorizationCode(config.oauth2);
 
@@ -158,6 +159,7 @@ async function parse_web(match, tournamentId) {
     const link_page = await axios.get(`https://organizer.toornament.com/tournaments/${tournamentId}/matches/${match.id}/league-of-legends-stats`, cookies);
     $ = cheerio.load(link_page.data);
     match.links = $("[id^='match_set_status_collection_statuses']").toArray().map((element) => { return element.attribs.value });
+
 }
 
 
@@ -178,6 +180,19 @@ async function riot_call(match) {
         }
     }
 }
+let ranks = {};
+async function get_rank(summonerId) {
+    if (ranks[summonerId])
+        return ranks[summonerId]
+    let res = await axios.get(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${riot_key}`);
+    let solo = res.data.filter(q => q.queueType == 'RANKED_SOLO_5x5');
+    if (solo.length != 0) {
+        ranks[summonerId] = solo[0].tier + ' ' + solo[0].rank;
+        return solo[0].tier + ' ' + solo[0].rank;
+    }
+    ranks[summonerId] = "";
+    return "";
+}
 
 
 async function add_team_info(opponent, player) {
@@ -189,6 +204,8 @@ async function add_team_info(opponent, player) {
         } else {
             if (response[0].players.filter((p) => p.accountId == player.accountId).length != 0) {
                 response[0].players.filter((p) => p.accountId == player.accountId)[0].summonerName = player.summonerName;
+                response[0].players.filter((p) => p.accountId == player.accountId)[0].soloRank = player.soloRank;
+
             } else
                 response[0].players.push(player)
             await r.db('GL5').table('teams').filter({ name: opponent.name }).update({ players: response[0].players }).run();
@@ -203,22 +220,27 @@ async function add_player_info(identity, player_perf, match, i) {
             identity.palier = match.tournament_name.split('-')[2].trim();
             identity.summonerName = identity.summonerName.trim();
             let n = 0;
+
             if (player_perf.stats.win)
                 n = match.games[i].opponents.filter(op => op.result == 'win')[0].number;
             else
                 n = match.games[i].opponents.filter(op => op.result == 'loss')[0].number;
             identity.participant = match.opponents.filter(op => op.number == n)[0].participant;
             identity.participant.region = identity.region;
+            identity.soloRank = await get_rank(identity.summonerId);
             identity.participant.palier = identity.palier;
             identity.games = [player_perf];
             r.db('GL5').table('playersFiltered').insert(identity).run();
-            await add_team_info(identity.participant, { summonerName: identity.summonerName, accountId: identity.accountId, profileIcon: identity.profileIcon })
+            await add_team_info(identity.participant, { summonerName: identity.summonerName, accountId: identity.accountId, profileIcon: identity.profileIcon, soloRank: identity.soloRank })
         } else {
             if (response[0].games.filter((game) => game.gameId == match.gameId) != 0)
                 return;
             response[0].games.push(player_perf)
-            await r.db('GL5').table('playersFiltered').filter({ accountId: identity.accountId }).update({ profileIcon: identity.profileIcon }).run();
-            await r.db('GL5').table('playersFiltered').filter({ accountId: identity.accountId }).update({ games: response[0].games }).run();
+            await r.db('GL5').table('playersFiltered').filter({ accountId: identity.accountId }).update({
+                profileIcon: identity.profileIcon,
+                games: response[0].games,
+                soloRank: await get_rank(identity.summonerId)
+            }).run();
         }
     });
 }
@@ -278,19 +300,20 @@ async function update_done_matches() {
         });
         console.log(response.length, "matches are in the database.");
     });
-
 }
 
 async function parseTournament(t, log) {
     match_fails = match_fails.filter(m => m != t);
     let t_info = await getTournamentInfo(t);
     let matches = await getMatches(t); //input.split(' ')[1]);
+    console.log(matches.length);
     matches = matches.filter((match) => match.status == 'completed');
-    console.log("Found", matches.length, "completed matches.");
+    console.log(matches.length);
+    console.log("Found", matches.length, "completed matches for tournament", t, ".");
     let i = 0;
     for (let match of matches) {
         i++;
-        
+
         match.tournament_name = t_info.name;
         match.tournament_id = t_info.id;
         match.tournament_full_name = t_info.full_name;
@@ -304,6 +327,151 @@ async function parseTournament(t, log) {
     }
     await filter_games(matches);
 }
+
+async function inspect() {
+    let coach = [];
+    let gt6 = [];
+    await r.db('GL5').table('teams').run().then(response => {
+        response.forEach(team => {
+            if (team.players.filter(p => p.summonerName == team.custom_fields.coach).length != 0)
+                coach.push(team);
+            if (team.players.length > 5)
+                gt6.push(team)
+        });
+    });
+    let content = "";
+    content += 'Coach played\n';
+    coach.forEach(team => {
+        content += `${team.name},${team.region},${team.palier},Coach,${team.custom_fields.coach},Players,${team.players.map(p => { return p.summonerName }).join(",")}\n`;
+    });
+    content += '\nMore than 5 players\n';
+    gt6.forEach(team => {
+        content += `${team.name},${team.region},${team.palier},Players,${team.players.map(p => { return p.summonerName }).join(",")}\n`;
+    });
+    fs.writeFile('inspection.csv', content, function () { });
+    console.log("Inspection done, check inspection.txt");
+}
+
+
+async function generate_statistics() {
+    let stats = {
+        totalKills: 0,
+        totalCS: 0,
+        totalGames: 0,
+        totalDamages: 0,
+        totalGoldSpent: 0,
+        kda: {
+            value: 0,
+            summonerName: ""
+        },
+        cs: {
+            value: 0,
+            summonerName: ""
+        },
+        visionScore: {
+            value: 0,
+            summonerName: ""
+        },
+        alive: {
+            value: 0,
+            summonerName: ""
+        },
+        damages: {
+            value: 0,
+            summonerName: ""
+        },
+        pentakills: {
+            players: []
+        }
+    };
+    let champions = {};
+    await r.db('GL5').table('playersFiltered').run().then(response => {
+        for (let player of response) {
+            let kills = 0;
+            let deaths = 0;
+            let assists = 0;
+            let kda = 0;
+            let visionScore = 0;
+            let cs = 0;
+            let damages = 0;
+            for (let game of player.games) {
+                if (champions[game.championId] === undefined) {
+                    champions[game.championId] = 0;
+                }
+                else
+                    champions[game.championId] += 1;
+
+                if (game.stats.largestMultiKill == 5) {
+                    console.log(player.summonerName);
+                    stats.pentakills.players.push({ summonerName: player.summonerName, profileIcon: player.profileIcon });
+                }
+                stats.totalGoldSpent += game.stats.goldSpent;
+                stats.totalKills += game.stats.kills;
+                stats.totalCS += game.stats.totalMinionsKilled + game.stats.neutralMinionsKilled;
+                stats.totalDamages += game.stats.totalDamageDealtToChampions;
+                cs += game.stats.totalMinionsKilled + game.stats.neutralMinionsKilled;
+                kills += game.stats.kills;
+                deaths += game.stats.deaths;
+                assists += game.stats.assists;
+                visionScore += game.stats.visionScore;
+                if (game.stats.longestTimeSpentLiving > stats.alive.value) {
+                    stats.alive.value = game.stats.longestTimeSpentLiving;
+                    stats.alive.summonerName = player.summonerName;
+                    stats.alive.profileIcon = player.profileIcon;
+                }
+                damages += game.stats.totalDamageDealtToChampions;
+                kda += (game.stats.kills + game.stats.assists) / (game.stats.deaths == 0 ? 1 : game.stats.deaths);
+            }
+            kda /= player.games.length;
+            if (kda > stats.kda.value) {
+                stats.kda.value = kda;
+                stats.kda.kills = kills / player.games.length;
+                stats.kda.deaths = deaths / player.games.length;
+                stats.kda.assists = assists / player.games.length;
+                stats.kda.summonerName = player.summonerName;
+                stats.kda.profileIcon = player.profileIcon;
+            }
+            visionScore /= player.games.length;
+            if (visionScore > stats.visionScore.value) {
+                stats.visionScore.value = visionScore;
+                stats.visionScore.summonerName = player.summonerName;
+                stats.visionScore.profileIcon = player.profileIcon;
+            }
+            cs /= player.games.length;
+            if (cs > stats.cs.value) {
+                stats.cs.value = cs;
+                stats.cs.summonerName = player.summonerName;
+                stats.cs.profileIcon = player.profileIcon;
+            }
+            damages /= player.games.length;
+            if (damages > stats.damages.value) {
+                stats.damages.value = damages;
+                stats.damages.summonerName = player.summonerName;
+                stats.damages.profileIcon = player.profileIcon;
+            }
+        }
+        let max = 0;
+        let champMax = 0;
+        console.log(champions);
+        for (let key in champions) {
+            if (champions[key] > max) {
+                max = champions[key];
+                champMax = key;
+            }
+        }
+        stats.popChampion = { id: champMax, value: max };
+
+    });
+    await r.db('GL5').table('matches').run().then(response => {
+        for (let match of response)
+            for (let game of match.data)
+                if (game)
+                    stats.totalGames += 1;
+    });
+    await r.db('GL5').table('statistics').delete().run();
+    r.db('GL5').table('statistics').insert(stats).run();
+}
+
 
 rl.on('line', async (input) => {
     if (input === "open") {
@@ -332,25 +500,24 @@ rl.on('line', async (input) => {
         await r.db('GL5').table('playersFiltered').delete().run();
         await r.db('GL5').table('teams').delete().run();
         console.log("Reset done.");
+    } else if (input.startsWith("inspect")) {
+        inspect();
+
+    } else if (input.startsWith("stats")) {
+        await generate_statistics();
+        console.log("Statistics generated.");
     }
 });
 
-/*r.db('GL5').table("matches").run().then(response => {
+/*
+r.db('GL5').table("matches").run().then(response => {
     for (let match of response) {
-        if (match.opponents.filter(op => op.participant.name == "Brest of 5").length != 0) {
+        if (match.opponents.filter(op => op.participant.name == "Les fiers de haches").length != 0) {
             console.log(match);
+            fs.appendFile('seek.json', JSON.stringify(match, null, 4), function(){});
 
-            match.data.forEach(d => {
-                if (d && false) {
-                    for (let i = 0; i < d.participantIdentities.length; i++) {
-                        if ((d.participants[i].stats.win && match.opponents.filter(op => op.participant.name == "Brest of 5")[0].result == 'win')
-                            || (!d.participants[i].stats.win && match.opponents.filter(op => op.participant.name == "Brest of 5")[0].result == 'loss')) {
-                            console.log(d.participantIdentities[i], d.participants[i].stats.win, match.opponents.filter(op => op.participant.name == "Brest of 5")[0].result);
-                        }
-                    }
-                }
-            });
+
         }
     }
-});*/
-
+});
+*/
